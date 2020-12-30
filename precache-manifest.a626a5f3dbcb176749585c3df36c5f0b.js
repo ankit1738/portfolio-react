cac4a7fd54d61c1bf55f4920ded4c10ee44b0519
Ankit <you@example.com>
2020-12-30 12:54:21 +0530

    Updates

---
+++ precache-manifest.a626a5f3dbcb176749585c3df36c5f0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771b77fac6f77e0f74cd6a8a4f242805",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "bec70b58296b9d0b6eb0",
+    "url": "/portfolio-react/static/css/main.b0f8397b.chunk.css"
+  },
+  {
+    "revision": "db03e3519eeb5cc6e40a",
+    "url": "/portfolio-react/static/js/2.95c9a01a.chunk.js"
+  },
+  {
+    "revision": "fa24b8794423abbcfcdccf71576deb88",
+    "url": "/portfolio-react/static/js/2.95c9a01a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bec70b58296b9d0b6eb0",
+    "url": "/portfolio-react/static/js/main.0c7b668e.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  }
+]);
